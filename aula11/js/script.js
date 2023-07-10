5314942aafaef7896a366da59ca38d24521180d0
lugialo <gabrielzin911@gmail.com>
2023-07-09 21:58:07 -0300

    Aula 11

--- aula11/js/script.js
+++ aula11/js/script.js
@@ -1,1 +1,5 @@
-alert("Com a nossa mensagem.");
+num1 = prompt("Digite o primeiro número: ");
+num2 = prompt("Digite o segundo número: ");
+
+const resultado = parseFloat(num1) + parseFloat(num2);
+alert(`O resultado foi: ${resultado}`);
