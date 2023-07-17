d28028691d3f9ccecc665219af490510c2c3d519
lugialo <gabrielzin911@gmail.com>
2023-07-17 09:59:32 -0300

    Aula 22

---
+++ aula22/index.js
@@ -0,0 +1,11 @@
+/*
+Operadores Lógicos
+&& -> AND -> E -> Todas as expressões precisam ser veradeiras para retornar true
+|| - > OR -> OU -> 
+! -> NOT -> NÃO
+*/
+
+const expressaoAnd = true && true && true && true;
+const expressaoOr = true || false; 
+console.log(expressaoAnd);
+console.log(expressaoOr);
