5310042abf8b7dee28783dd204139b22e391c3d6
Gabriel Antonin <gabrielantonin999@gmail.com>
2023-12-17 11:13:28 -0300

    Aulas 54 e 55.

---
+++ aula55/aula55.js
@@ -0,0 +1,13 @@
+// IIFE -> Immediately invoked function expression
+
+(function(idade, peso, altura) {
+    const sobrenome = 'José';
+    function criaNome(nome) {
+        return nome + ' ' + sobrenome;
+    }
+   function falaNome() {
+    console.log(criaNome('Luiz'));
+   } 
+   falaNome();
+   console.log(idade, peso, altura);
+})(30, 80, 1.80);
