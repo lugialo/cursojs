eb749a22e2b16454ce09d87377edc6a30d2f1837
Gabriel Antonin Pascoali <gabrielantonin999@gmail.com>
2024-01-08 13:56:15 -0300

    Aulas 63, 64 e 65.

---
+++ aula63/aula63.js
@@ -0,0 +1,7 @@
+//               -5      -4       -3          -2        -1
+//                0       1        2           3         4
+const nomes = ['Maria', 'João', 'Eduardo', 'Gabriel', 'Júlia'];
+
+const removidos = nomes.splice(4,1);
+
+console.log(nomes, removidos);
