5adadcd5fba8475df9d1a340662df4343782ffc8
Gabriel Antonin Pascoali <gabrielantonin999@gmail.com>
2024-01-22 14:04:43 -0300

    Aulas 74, 75, 76 e 77.

---
+++ aula74/aula74.js
@@ -0,0 +1,31 @@
+/*
+JavaScript é baseado em protótipos para passar propriedades e métodos de um objeto para outro.
+
+Definição de protótipo
+Protótipo é o termo usado para se referir ao que foi criado pela
+primeira vez, servindo de modelo ou molde para futuras produções.
+
+Todos os objetos tem uma referência interna para um protótipo (__proto__)
+que vem da propriedade prototype da função construtora que foi usada para
+criá-lo. Quando tentamos acessar um membro de um objeto, primeiro o motor
+do JS vai tentar encontrar este membro no próprio objeto e depois a cadeia
+de protótipos é usada até o topo (null) até encontrar (ou não) tal membro.
+*/
+
+// Construtora -> molde (classe)
+function Pessoa(nome, sobrenome) {
+    this.nome = nome;
+    this.sobrenome = sobrenome;
+    this.nomeCompleto = () => this.nome + ' ' + this.sobrenome;
+}
+
+Pessoa.prototype.nomeCompleto = function() {
+    return this.nome + ' ' + this.sobrenome;
+  };
+// instância
+
+const pessoa1 = new Pessoa('Luiz', 'O.')// <- Pessoa = função construtora
+const pessoa2 = new Pessoa('Maria', 'A.');// <- Pessoa = função construtora
+
+console.dir(pessoa1);
+console.dir(pessoa2);
---
+++ aula76/aula76.js
@@ -0,0 +1,38 @@
+// Produto -> aumento, desconto
+// Camiseta = Cor, caneca = material, lápis
+
+function Produto(nome,preco) {
+    this.nome = nome;
+    this.preco = preco;
+}
+
+Produto.prototype.aumento = function(quantia) {
+    this.preco += quantia;
+}
+
+Produto.prototype.desconto = function(quantia) {
+    this.preco -= quantia;
+}
+
+function Camiseta(nome, preco, cor) {
+    Produto.call(this, nome, preco);
+}
+
+Camiseta.prototype.aumento = function(percentual) {
+    this.preco = this.preco + (this.preco * (percentual / 100));
+}
+
+function Caneca(nome, preco, material) {
+    Produto.call(this, nome, preco);
+    this.material = material;
+}
+
+Caneca.prototype = Object.create(Produto.prototype);
+Caneca.prototype.constructor = Caneca;
+
+Camiseta.prototype = Object.create(Produto.prototype);
+Camiseta.prototype.constructor = Camiseta;
+
+const camiseta = new Camiseta('Regata', 7.5, 'Preta');
+camiseta.aumento(10);
+console.log(camiseta)
