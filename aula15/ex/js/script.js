5942a723ac8cab9e0da6287fde4e260b382a9ddc
Gabriel Antonin Pascoali <gabrielantonin999@gmail.com>
2024-02-02 15:20:29 -0300

    Aula 15 - Exercício utilizando o DOM e os métodos dos Numbers

--- aula15/ex/js/script.js
+++ aula15/ex/js/script.js
@@ -2,6 +2,7 @@ const numero = Number(prompt('Digite um número:'));
 const numeroTitulo = document.getElementById('numero-titulo');
 const texto = document.getElementById('texto');
 
+
 numeroTitulo.innerHTML = numero;
 texto.innerHTML += `<p>Raiz quadrada: ${Math.sqrt(numero)}</p>`;
 texto.innerHTML += `<p>${numero} é inteiro: ${Number.isInteger(numero)}`
