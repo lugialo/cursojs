682226758ecf8e7cf2ef0754b8de3ba09badc736
Gabriel Antonin Pascoali <gabrielantonin999@gmail.com>
2024-02-04 17:24:54 -0300

    Aula 37 - for of

--- aula37/index.js
+++ aula37/index.js
@@ -6,6 +6,7 @@ for (let valor of nome) {
     
 }
 
+
 // For clássico - Geralmente com iteráveis (array ou string)
 // For in - retorna o índice ou chave (string, array ou objetos)
 // For of - Retorna o valor em si (iteráveis, arrays ou strings)
