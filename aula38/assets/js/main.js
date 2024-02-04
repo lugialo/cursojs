076dea0ce8c367e88c37e2f7054184bf0f806ab2
Gabriel Antonin Pascoali <gabrielantonin999@gmail.com>
2024-02-04 17:25:35 -0300

    Aula 38 - Exercícios com node list

--- aula38/assets/js/main.js
+++ aula38/assets/js/main.js
@@ -5,6 +5,7 @@ const estilosBody = getComputedStyle(document.body);
 const backgroundColorBody = estilosBody.backgroundColor;
 console.log(backgroundColorBody)
 
+
 for (let p of ps) {
   p.style.backgroundColor = backgroundColorBody;
   p.style.color = '#FFFFFF';
