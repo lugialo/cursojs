32da7906223d3ba6977da083e049823dd0cb8ba3
Gabriel Antonin Pascoali <gabrielantonin999@gmail.com>
2024-02-08 17:45:27 -0300

    Aula 66 - Map - mapeando o array

--- aula66/aula66.js
+++ aula66/aula66.js
@@ -2,7 +2,7 @@
 //
 const numeros = [5, 50, 80, 1, 2, 3, 4, 5, 6, 7, 11, 15, 22];
 
-const numerosEmDobro = numeros.map(valor => valor * 2);
+const numerosEmDobro = numeros.map((valor) => valor * 2);
 console.log(numerosEmDobro);
 
 // Para cada elemento:
@@ -11,21 +11,20 @@ console.log(numerosEmDobro);
 // Adicione uma chave id em cada objeto
 
 const pessoas = [
-    { nome: 'Luiz', idade: 52 },
-    { nome: 'José', idade: 43 },
-    { nome: 'Bruno', idade: 22 },
-    { nome: 'Maria', idade: 25 },
-    { nome: 'Rosana', idade: 32 },
-    { nome: 'Wallace', idade: 29 },
-]
+  { nome: "Luiz", idade: 52 },
+  { nome: "José", idade: 43 },
+  { nome: "Bruno", idade: 22 },
+  { nome: "Maria", idade: 25 },
+  { nome: "Rosana", idade: 32 },
+  { nome: "Wallace", idade: 29 },
+];
 
-const nome = pessoas.map(pessoa => pessoa.nome);
+const nome = pessoas.map((pessoa) => pessoa.nome);
 console.log(nome);
 
-const idades = pessoas.map(obj => ({idade: obj.idade}));
+const idades = pessoas.map((obj) => ({ idade: obj.idade }));
 console.log(idades);
 
 const comIds = pessoas.map((obj, indice) => ({ indice, ...obj }));
 
 console.log(comIds);
-
