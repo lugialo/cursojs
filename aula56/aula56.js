33b3407d038a1d13a2fdbef5dc4fe2a752546515
Gabriel Antonin Pascoali <gabrielantonin999@gmail.com>
2024-02-08 16:15:37 -0300

    Aula 56 - Factory functions

--- aula56/aula56.js
+++ aula56/aula56.js
@@ -1,33 +1,33 @@
 function criaPessoa(nome, sobrenome, altura, peso) {
-    return {
-        nome,
-        sobrenome,
+  return {
+    nome,
+    sobrenome,
 
-        // Getter
-        get nomeCompleto() {
-            return `${this.nome} ${this.sobrenome}`;
-        },
+    // Getter
+    get nomeCompleto() {
+      return `${this.nome} ${this.sobrenome}`;
+    },
 
-        // Setter
+    // Setter
 
-        set nomeCompleto(valor) {
-            valor = valor.split(' '); // Split the string into an array
-            this.nome = valor.shift(); 
-            this.sobrenome = valor.join(' ');
-        },
-        fala(assunto) {
-            return `${this.nome} ${this.sobrenome} está falando sobre ${assunto}.`; 
-        },
-        altura: altura,
-        peso: peso,
-        // Getter
-        get imc () {
-            const indice = this.peso / (this.altura ** 2);
-            return indice.toFixed(2);
-        }
-    };
+    set nomeCompleto(valor) {
+      valor = valor.split(" "); // Split the string into an array
+      this.nome = valor.shift();
+      this.sobrenome = valor.join(" ");
+    },
+    fala(assunto) {
+      return `${this.nome} ${this.sobrenome} está falando sobre ${assunto}.`;
+    },
+    altura: altura,
+    peso: peso,
+    // Getter
+    get imc() {
+      const indice = this.peso / this.altura ** 2;
+      return indice.toFixed(2);
+    },
+  };
 }
 
-const p1 = criaPessoa('Gabriel', 'Antonin', 1.80, 80);
-p1.nomeCompleto = 'João Bolão';
-console.log(p1.nome)
+const p1 = criaPessoa("Gabriel", "Antonin", 1.8, 80);
+p1.nomeCompleto = "João Bolão";
+console.log(p1.nome);
