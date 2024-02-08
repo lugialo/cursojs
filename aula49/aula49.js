adba3955acdeaa4006a508762ccb2e2d68fff425
Gabriel Antonin Pascoali <gabrielantonin999@gmail.com>
2024-02-08 12:56:19 -0300

    Aula 49 - Maneiras diferentes de declarar uma função

--- aula49/aula49.js
+++ aula49/aula49.js
@@ -4,36 +4,31 @@
 
 falaOi();
 function falaOi() {
-    console.log('Oie');
+  console.log("Oie");
 }
-
 // First-class objects (Objetos de primeira classe)
 // Function expression
 
-const souUmDado = function() {
-    console.log('Sou um dado.');
+const souUmDado = function () {
+  console.log("Sou um dado.");
 };
 
 function executaFuncao(funcao) {
-    console.log('Vou executar sua função abaixo.');
+  console.log("Vou executar sua função abaixo.");
 }
 executaFuncao(souUmDado);
 
 // Arrow function
 const funcaoArrow = () => {
-    console.log('Sou uma arrow function');
-}
+  console.log("Sou uma arrow function");
+};
 funcaoArrow();
 
 //dentro de um objeto
 const obj = {
-    falar: function() {
-        console.log('Estou falando...');
-    }
-}
+  falar: function () {
+    console.log("Estou falando...");
+  },
+};
 
 obj.falar();
-
-
-
-
