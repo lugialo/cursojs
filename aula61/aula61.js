1d9b700eab95afba26471f0df3ed3af014bd58ad
Gabriel Antonin Pascoali <gabrielantonin999@gmail.com>
2024-02-08 17:41:51 -0300

    Aula 61 - Funções Geradoras

--- aula61/aula61.js
+++ aula61/aula61.js
@@ -1,47 +1,46 @@
 function* geradora1() {
-    // Código qualquer
-    yield 'Valor 1';
-    // Código qualquer
-    yield 'Valor 2';
-    // Código qualquer
-    yield 'Valor 3';
+  // Código qualquer
+  yield "Valor 1";
+  // Código qualquer
+  yield "Valor 2";
+  // Código qualquer
+  yield "Valor 3";
 }
 
 function* geradora2() {
-    let i = 0;
+  let i = 0;
 
-    while(true) {
-        yield i;
-        i++;
-    }
+  while (true) {
+    yield i;
+    i++;
+  }
 }
 
 function* geradora3() {
-    yield 0;
-    yield 1;
-    yield 2;
+  yield 0;
+  yield 1;
+  yield 2;
 }
 
 function* geradora4() {
-    yield* geradora3();
-    yield 3;
-    yield 4;
-    yield 5;
+  yield* geradora3();
+  yield 3;
+  yield 4;
+  yield 5;
 }
 const g4 = geradora4();
 for (let valor of g4) {
-    console.log(valor);
+  console.log(valor);
 }
 
 function* geradora5() {
-    yield function() {
-        console.log('Vim do y1');
+  yield function () {
+    console.log("Vim do y1");
+  };
 
-    };
-
-    yield function() {
-        console.log('Vim do y2');
-    }
+  yield function () {
+    console.log("Vim do y2");
+  };
 }
 const g5 = geradora5();
 const func1 = g5.next().value;
