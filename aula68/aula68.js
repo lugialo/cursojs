07fef8664723e2670cb594acae4becfb1d1c4806
Gabriel Antonin Pascoali <gabrielantonin999@gmail.com>
2024-02-08 17:46:15 -0300

    Aula 68 - Filter + Map + Reduce

--- aula68/aula68.js
+++ aula68/aula68.js
@@ -5,8 +5,8 @@
 
 const numeros = [5, 50, 80, 1, 2, 3, 4, 5, 6, 7, 11, 15, 22];
 const numerosPares = numeros
-    .filter(valor => valor % 2 === 0)
-    .map(valor => valor * 2 )
-    .reduce((ac, valor) => ac + valor);
+  .filter((valor) => valor % 2 === 0)
+  .map((valor) => valor * 2)
+  .reduce((ac, valor) => ac + valor);
 
 console.log(numerosPares);
