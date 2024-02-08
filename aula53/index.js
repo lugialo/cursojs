2bb051a1d4584223a485faa31c1ff894edf8d1b1
Gabriel Antonin Pascoali <gabrielantonin999@gmail.com>
2024-02-08 16:12:51 -0300

    Aula 53 - Closures

--- aula53/index.js
+++ aula53/index.js
@@ -1,8 +1,9 @@
-function retornaFuncao() { // Closure = habilidade da função de acessar seu escopo léxico
-    const nome = 'Luiz';
-    return function() {
-        return nome;
-    };
+function retornaFuncao() {
+  // Closure = habilidade da função de acessar seu escopo léxico
+  const nome = "Luiz";
+  return function () {
+    return nome;
+  };
 }
 
 const funcao = retornaFuncao();
