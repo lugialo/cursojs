16ad429807a5b276c0d462a8a36f891ddd0045ad
Gabriel Antonin Pascoali <gabrielantonin999@gmail.com>
2024-02-08 17:48:16 -0300

    Aula 70 - Revisando objetos

--- aula70/aula70.js
+++ aula70/aula70.js
@@ -1,27 +1,27 @@
 //Revisão de objetos
 
 const pessoa1 = new Object(); // Método construtor
-pessoa1.nome = 'Luiz';
-pessoa1.sobrenome = 'Otávio';
+pessoa1.nome = "Luiz";
+pessoa1.sobrenome = "Otávio";
 pessoa1.idade = 30;
-
 
 delete pessoa1.nome;
 console.log(pessoa1);
 
-const pessoa = { // Método literal
-    nome: 'Gabriel',
-    sobrenome: 'Antonin',
-}
+const pessoa = {
+  // Método literal
+  nome: "Gabriel",
+  sobrenome: "Antonin",
+};
 
-pessoa1.falarNome = function() {
-    return(`${this.nome} está falando seu nome`); // Podemos usar this pra referenciar o objeto
-}
+pessoa1.falarNome = function () {
+  return `${this.nome} está falando seu nome`; // Podemos usar this pra referenciar o objeto
+};
 
-pessoa1.getDataNascimento = function() {
-    const dataAtual = new Date();
-    return dataAtual.getFullYear() - this.idade;
-}
+pessoa1.getDataNascimento = function () {
+  const dataAtual = new Date();
+  return dataAtual.getFullYear() - this.idade;
+};
 
 console.log(pessoa1.getDataNascimento());
 
