36227783564a09006e65672d37dcd8d3efed9b8d
Gabriel Antonin Pascoali <gabrielantonin999@gmail.com>
2024-02-08 17:45:50 -0300

    Aula 67 - Reduce - reduzindo o array

--- aula67/aula67.js
+++ aula67/aula67.js
@@ -1,4 +1,3 @@
-
 // Some todos os números (reduce)
 // Retorne um array com os pares (filter)
 // Retorne um array com o dobro dos valores (Map)
@@ -11,23 +10,23 @@ const numeros = [5, 50, 80, 1, 2, 3, 4, 5, 6, 7, 11, 15, 22];
 // console.log(total);
 
 const total = numeros.reduce(function (acumulador, valor, indice, array) {
-    acumulador.push(valor * 2);
-    return acumulador;
+  acumulador.push(valor * 2);
+  return acumulador;
 }, []);
 console.log(total);
 
 // Retorne a pessoa mais velha
 
 const pessoas = [
-    { nome: 'Luiz', idade: 52 },
-    { nome: 'José', idade: 43 },
-    { nome: 'Bruno', idade: 22 },
-    { nome: 'Maria', idade: 25 },
-    { nome: 'Rosana', idade: 32 },
-    { nome: 'Wallace', idade: 29 },
-]
+  { nome: "Luiz", idade: 52 },
+  { nome: "José", idade: 43 },
+  { nome: "Bruno", idade: 22 },
+  { nome: "Maria", idade: 25 },
+  { nome: "Rosana", idade: 32 },
+  { nome: "Wallace", idade: 29 },
+];
 
-const maisVelha = pessoas.reduce(function(acumulador, valor) {
-    if (acumulador.idade > valor.idade) return acumulador;
-}); 
+const maisVelha = pessoas.reduce(function (acumulador, valor) {
+  if (acumulador.idade > valor.idade) return acumulador;
+});
 console.log(maisVelha);
