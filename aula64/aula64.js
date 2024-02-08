da24ce70e82193b111cdae1f466a75ddc9abc5ae
Gabriel Antonin Pascoali <gabrielantonin999@gmail.com>
2024-02-08 17:43:36 -0300

    Aula 64 - concatenando arrays

--- aula64/aula64.js
+++ aula64/aula64.js
@@ -1,5 +1,5 @@
-const a1 = [1,2,3];
-const a2 = [4,5,6];
+const a1 = [1, 2, 3];
+const a2 = [4, 5, 6];
 // const a3 = a1.concat(a2);
-const a3 = [...a1, 'Bolinha', ...a2];
+const a3 = [...a1, "Bolinha", ...a2];
 console.log(a3);
