54ed204dc839c5d2906fcc1bc0ca952c81a2cea6
Gabriel Antonin Pascoali <gabrielantonin999@gmail.com>
2024-02-08 16:13:21 -0300

    Aula 54 - Funções de Callback

--- aula54/aula54.js
+++ aula54/aula54.js
@@ -1,26 +1,26 @@
 // Funções de callback
 
 function rand(min = 1000, max = 3000) {
-    const num = Math.random() * (max - min) + min;
-    return Math.floor(num);
+  const num = Math.random() * (max - min) + min;
+  return Math.floor(num);
 }
-function f1(callback)  {
-    setTimeout(function() {
-        console.log('f1');
-        if (callback) callback();
-    }, rand());
-}                   
-function f2(callback)  {
-    setTimeout(function() {
-        console.log('f2');
-        if (callback) callback();
-    }, rand());
+function f1(callback) {
+  setTimeout(function () {
+    console.log("f1");
+    if (callback) callback();
+  }, rand());
 }
-function f3(callback)  {
-    setTimeout(function() {
-        console.log('f3');
-        if (callback) callback();
-    }, rand());
+function f2(callback) {
+  setTimeout(function () {
+    console.log("f2");
+    if (callback) callback();
+  }, rand());
+}
+function f3(callback) {
+  setTimeout(function () {
+    console.log("f3");
+    if (callback) callback();
+  }, rand());
 }
 
 // f1(function() {
@@ -32,13 +32,13 @@ function f3(callback)  {
 // });
 
 function f1Callback() {
-    f2(f2Callback);
-};
+  f2(f2Callback);
+}
 
 function f2Callback() {
-    f3(f3Callback);
+  f3(f3Callback);
 }
 
 function f3Callback() {
-    console.log('Olá mundo!');
+  console.log("Olá mundo!");
 }
