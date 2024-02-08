9db40fef96598331c444b42e12e45628e876d496
Gabriel Antonin Pascoali <gabrielantonin999@gmail.com>
2024-02-08 16:14:40 -0300

    Aula 55 - Funções Imediatas - IIFE

--- aula55/aula55.js
+++ aula55/aula55.js
@@ -1,13 +1,13 @@
 // IIFE -> Immediately invoked function expression
 
-(function(idade, peso, altura) {
-    const sobrenome = 'José';
-    function criaNome(nome) {
-        return nome + ' ' + sobrenome;
-    }
-   function falaNome() {
-    console.log(criaNome('Luiz'));
-   } 
-   falaNome();
-   console.log(idade, peso, altura);
-})(30, 80, 1.80);
+(function (idade, peso, altura) {
+  const sobrenome = "José";
+  function criaNome(nome) {
+    return nome + " " + sobrenome;
+  }
+  function falaNome() {
+    console.log(criaNome("Luiz"));
+  }
+  falaNome();
+  console.log(idade, peso, altura);
+})(30, 80, 1.8);
