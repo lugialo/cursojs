e06866038a6ebdfc3b176f180fffc88689c8798f
Gabriel Antonin Pascoali <gabrielantonin999@gmail.com>
2024-02-08 17:17:59 -0300

    Aula 58 - Funções construtoras

--- aula58/aula58.js
+++ aula58/aula58.js
@@ -2,23 +2,21 @@
 // Função fábrica -> objetos
 // Construtora -> Pessoa (new)
 function Pessoa(nome, sobrenome) {
-    // Atributos ou métodos privados
-    const ID = 123456;
+  // Atributos ou métodos privados
+  const ID = 123456;
 
-    const metodoInterno = function() {
+  const metodoInterno = function () {};
+  // Atributos ou métodos públicos
+  this.nome = nome;
+  this.sobrenome = sobrenome;
 
-    };
-    // Atributos ou métodos públicos
-    this.nome = nome;
-    this.sobrenome = sobrenome;
-
-    this.metodo = function() {
-        console.log(this.nome + ': sou um método');
-    }
+  this.metodo = function () {
+    console.log(this.nome + ": sou um método");
+  };
 }
 
-const p1 = new Pessoa('Gabriel', 'Antonin');
-const p2 = new Pessoa('José', 'Felipe');
-p1.metodo
+const p1 = new Pessoa("Gabriel", "Antonin");
+const p2 = new Pessoa("José", "Felipe");
+p1.metodo;
 
 console.log(p1.nome);
