c677f80c517fb4342dfed26f2bb13c7f37794517
Gabriel Antonin Pascoali <gabrielantonin999@gmail.com>
2024-02-21 23:51:42 -0300

    Aula 90 - Fetch API

---
+++ aula90/assets/js/main.js
@@ -0,0 +1,31 @@
+document.addEventListener("click", (e) => {
+  const el = e.target;
+  const tag = el.tagName.toLowerCase();
+
+  if (tag === "a") {
+    e.preventDefault();
+    carregaPagina(el);
+  }
+});
+
+async function carregaPagina(el) {
+  const href = el.getAttribute("href");
+  const response = await fetch(href);
+
+  if (response.status != 200) throw new Error("Erro 404");
+  const html = await response.text();
+  carregaResultado(html);
+}
+
+function carregaResultado(response) {
+  const resultado = document.querySelector(".resultado");
+  resultado.innerHTML = response;
+}
+
+fetch("pagina2.html")
+  .then((resposta) => {
+    if (resposta.status !== 200) throw new Error("Erro!");
+    return resposta.text();
+  })
+  .then((html) => console.log(html))
+  .catch((e) => console.error(e));
