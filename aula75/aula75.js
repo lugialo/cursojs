314a776594216dc73ffb33baeed018171953eef4
Gabriel Antonin Pascoali <gabrielantonin999@gmail.com>
2024-02-22 13:26:42 -0300

    Aula 75 - Manipulando Prototypes

--- aula75/aula75.js
+++ aula75/aula75.js
@@ -1,16 +1,16 @@
 function Produto(nome, preco) {
-  this.nome = nome;
-  this.preco = preco;
+    this.nome = nome;
+    this.preco = preco;
 }
 
-Produto.prototype.desconto = function (percentual) {
-  this.preco = this.preco - this.preco * (percentual / 10);
-};
+Produto.prototype.desconto = function(percentual) {
+    this.preco = this.preco - (this.preco * (percentual / 10));
+}
 
-Produto.prototype.desconto = function (percentual) {
-  this.preco = this.preco + this.preco * (percentual / 10);
-};
+Produto.prototype.desconto = function(percentual) {
+    this.preco = this.preco + (this.preco * (percentual / 10));
+}
 
-const p1 = new Produto("Camiseta", 50);
+const p1 = new Produto('Camiseta', 50);
 p1.desconto(100);
 p1.aumento(100);
