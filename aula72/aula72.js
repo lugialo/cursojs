d3be1a2db16b6e1ffb904085cca3212a171d3ee0
Gabriel Antonin Pascoali <gabrielantonin999@gmail.com>
2024-02-22 13:22:18 -0300

    Aula 72 - Getters e Setters

--- aula72/aula72.js
+++ aula72/aula72.js
@@ -1,18 +1,18 @@
 function Produto(nome, preco, estoque) {
-    this.nome = nome;
-    this.preco = preco;
+  this.nome = nome;
+  this.preco = preco;
 
-    Object.defineProperty(this, 'estoque', {
-        enumerable: true, // Mostra a chave
-        configurable: true, // configurável
-        get: function() {
-            return estoque;
-        },
-        set: function(valor) {
-            console.log(valor);
-        }
-    });
+  Object.defineProperty(this, "estoque", {
+    enumerable: true, // Mostra a chave
+    configurable: true, // configurável
+    get: function () {
+      return estoque;
+    },
+    set: function (valor) {
+      console.log(valor);
+    },
+  });
 }
 
-const p1 = new Produto('Camiseta', 20, 3);
+const p1 = new Produto("Camiseta", 20, 3);
 console.log(p1.estoque);
