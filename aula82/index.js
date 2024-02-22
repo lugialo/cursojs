18e32bba7fa41125fbd55bfab4ce5df21d4fe1bc
Gabriel Antonin Pascoali <gabrielantonin999@gmail.com>
2024-02-22 13:54:09 -0300

    Aula 82 - Herança com classes

--- aula82/index.js
+++ aula82/index.js
@@ -1,37 +1,35 @@
 class DispositivoEletronico {
-    constructor(nome) {
-        this.nome = nome;
-        this.ligado = false;
+  constructor(nome) {
+    this.nome = nome;
+    this.ligado = false;
+  }
+
+  ligar() {
+    if (this.ligado) {
+      console.log(this.nome + " já ligado.");
+      return;
     }
 
-    ligar() {
-        if (this.ligado) {
-            console.log(this.nome + ' já ligado.');
-            return
-        }
+    this.ligado = true;
+  }
 
-        this.ligado = true;
+  desligar() {
+    if (!this.ligado) {
+      console.log(this.nome + " já desligado.");
+      return;
     }
 
-    desligar() {
-        if (!this.ligado) {
-            console.log(this.nome + ' já desligado.');
-            return
-        }
-
-        this.ligado = false;
-
-    }
+    this.ligado = false;
+  }
 }
 
 class Smartphone extends DispositivoEletronico {
-    constructor(nome, cor, modelo) {
-        super(nome); // Chama o construtor da classe pai
-        this.cor = cor;
-        this.modelo = modelo;
-    }   
-
+  constructor(nome, cor, modelo) {
+    super(nome); // Chama o construtor da classe pai
+    this.cor = cor;
+    this.modelo = modelo;
+  }
 }
 
-const s1 = new Smartphone('nokia tijolasso' , 'preto', 'inquebrável');
+const s1 = new Smartphone("nokia tijolasso", "preto", "inquebrável");
 console.log(s1);
