4e41c85e24e06ec7324b08ef39a79f4b60610c10
Gabriel Antonin Pascoali <gabrielantonin999@gmail.com>
2024-02-22 13:22:55 -0300

    Aula 73 - Métodos úteis para objetos

--- aula73/aula73.js
+++ aula73/aula73.js
@@ -13,13 +13,14 @@ Object.defineProperty (define uma propriedade)
 */
 
 const produto = {
-    nome: 'Caneca', preco: 1.8
+  nome: "Caneca",
+  preco: 1.8,
 };
 
 // const outraCoisa = {...produto, material: 'porcelana'};
-const caneca = Object.assign({}, produto, {material: 'porcelana'});
+const caneca = Object.assign({}, produto, { material: "porcelana" });
 
-outraCoisa.nome = 'Outro nome';
+outraCoisa.nome = "Outro nome";
 outraCoisa.preco = 2.5;
 console.log(produto);
 console.log(outraCoisa);
