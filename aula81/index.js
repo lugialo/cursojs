bc3000360a4ee2ea3e7f932f0e58e215058d9fb3
Gabriel Antonin Pascoali <gabrielantonin999@gmail.com>
2024-02-22 13:53:30 -0300

    Aula 81 - Getters e Setters em Classes

--- aula81/index.js
+++ aula81/index.js
@@ -1,37 +1,36 @@
-const _velocidade = Symbol('[_velocidade]'); // não é necessário
+const _velocidade = Symbol("[_velocidade]"); // não é necessário
 
 class Carro {
-    constructor(nome) {
-        this.nome = nome;
-        this[_velocidade] = 0;
-    }
+  constructor(nome) {
+    this.nome = nome;
+    this[_velocidade] = 0;
+  }
 
-    set velocidade(valor) {
-        if (typeof valor !== 'number') return;
-        if (valor >= 100 || valor <= 0)return;
-        this[_velocidade] = valor;
-    }
+  set velocidade(valor) {
+    if (typeof valor !== "number") return;
+    if (valor >= 100 || valor <= 0) return;
+    this[_velocidade] = valor;
+  }
 
-    get velocidade() {
-        return this[_velocidade];
-    }
+  get velocidade() {
+    return this[_velocidade];
+  }
 
-    acelerar() {
-        if(this[_velocidade] >= 100) return;
-        this[_velocidade]++;
-    }
+  acelerar() {
+    if (this[_velocidade] >= 100) return;
+    this[_velocidade]++;
+  }
 
-    freiar() {
-        if(this[_velocidade] <= 0) return;
-        this[_velocidade]--;
-    }
+  freiar() {
+    if (this[_velocidade] <= 0) return;
+    this[_velocidade]--;
+  }
 }
 
-const c1 = new Carro('Fusca');
+const c1 = new Carro("Fusca");
 
-for(let i = 0; i <= 50; i++) {
-    c1.acelerar();
+for (let i = 0; i <= 50; i++) {
+  c1.acelerar();
 }
 
 console.log(c1);
-
