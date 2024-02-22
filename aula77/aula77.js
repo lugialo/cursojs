6e9eaa2c6c48964213c82ead9841685462c42834
Gabriel Antonin Pascoali <gabrielantonin999@gmail.com>
2024-02-22 13:28:52 -0300

    Aula 77 - Validando um CPF

--- aula77/aula77.js
+++ aula77/aula77.js
@@ -1,49 +1,48 @@
 function ValidaCPF(cpfEnviado) {
-    Object.defineProperty(this, 'cpfLimpo', {
-        enumerable: true,
-        get: function() {
-            return cpfEnviado.replace(/\D+/g, '');
-        }
-    });
+  Object.defineProperty(this, "cpfLimpo", {
+    enumerable: true,
+    get: function () {
+      return cpfEnviado.replace(/\D+/g, "");
+    },
+  });
 }
 
-ValidaCPF.prototype.valida = function() {
-    if(typeof this.cpfLimpo === 'undefined') return false;
-    if(this.cpfLimpo.length !== 11) return false;
-    if (this.isSequencia()) return false;
+ValidaCPF.prototype.valida = function () {
+  if (typeof this.cpfLimpo === "undefined") return false;
+  if (this.cpfLimpo.length !== 11) return false;
+  if (this.isSequencia()) return false;
 
-    const cpfParcial = this.cpfLimpo.slice(0, -2);
-    const digito1 = this.criaDigito(cpfParcial); 
-    const digito2 = this.criaDigito(cpfParcial + digito1);
+  const cpfParcial = this.cpfLimpo.slice(0, -2);
+  const digito1 = this.criaDigito(cpfParcial);
+  const digito2 = this.criaDigito(cpfParcial + digito1);
 
-    const novoCpf = cpfParcial + digito1 + digito2;
-    return novoCpf === this.cpfLimpo;
+  const novoCpf = cpfParcial + digito1 + digito2;
+  return novoCpf === this.cpfLimpo;
 };
 
-ValidaCPF.prototype.criaDigito = function(cpfParcial) {
-    const cpfArray = Array.from(cpfParcial);
+ValidaCPF.prototype.criaDigito = function (cpfParcial) {
+  const cpfArray = Array.from(cpfParcial);
 
-    let regressivo = cpfArray.length + 1;
-    const total = cpfArray.reduce((ac, val) => {
-        ac += (regressivo * Number(val));
-        regressivo--;
-        return ac;
-    }, 0);
+  let regressivo = cpfArray.length + 1;
+  const total = cpfArray.reduce((ac, val) => {
+    ac += regressivo * Number(val);
+    regressivo--;
+    return ac;
+  }, 0);
 
-    const digito = 11 - (total % 11);
-    return digito > 9 ? '0' : String(digito);
-
+  const digito = 11 - (total % 11);
+  return digito > 9 ? "0" : String(digito);
 };
 
-ValidaCPF.prototype.isSequencia = function() {
-    const sequencia = this.cpfLimpo[0].repeat(this.cpfLimpo.length)
-    console.log(sequencia === this.cpfLimpo);
+ValidaCPF.prototype.isSequencia = function () {
+  const sequencia = this.cpfLimpo[0].repeat(this.cpfLimpo.length);
+  console.log(sequencia === this.cpfLimpo);
 };
 
-const cpf = new ValidaCPF('705.484.450-52');
+const cpf = new ValidaCPF("705.484.450-52");
 
 if (cpf.valida()) {
-    console.log('Cpf válido!');
+  console.log("Cpf válido!");
 } else {
-    console.log('Cpf inválido!');
+  console.log("Cpf inválido!");
 }
