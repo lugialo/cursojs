1b7d63c4d92d818a2eedb25210d9e01d3f95c55c
Gabriel Antonin Pascoali <gabrielantonin999@gmail.com>
2024-02-22 13:26:16 -0300

    Aula 74 - Prototypes

--- aula74/aula74.js
+++ aula74/aula74.js
@@ -14,18 +14,18 @@ de protótipos é usada até o topo (null) até encontrar (ou não) tal membro.
 
 // Construtora -> molde (classe)
 function Pessoa(nome, sobrenome) {
-  this.nome = nome;
-  this.sobrenome = sobrenome;
-  this.nomeCompleto = () => this.nome + " " + this.sobrenome;
+    this.nome = nome;
+    this.sobrenome = sobrenome;
+    this.nomeCompleto = () => this.nome + ' ' + this.sobrenome;
 }
 
-Pessoa.prototype.nomeCompleto = function () {
-  return this.nome + " " + this.sobrenome;
-};
+Pessoa.prototype.nomeCompleto = function() {
+    return this.nome + ' ' + this.sobrenome;
+  };
 // instância
 
-const pessoa1 = new Pessoa("Luiz", "O."); // <- Pessoa = função construtora
-const pessoa2 = new Pessoa("Maria", "A."); // <- Pessoa = função construtora
+const pessoa1 = new Pessoa('Luiz', 'O.')// <- Pessoa = função construtora
+const pessoa2 = new Pessoa('Maria', 'A.');// <- Pessoa = função construtora
 
 console.dir(pessoa1);
 console.dir(pessoa2);
