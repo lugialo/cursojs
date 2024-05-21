fd6abb5eeac07b64b12e6f8a4298f5b8b9e0aef2
Gabriel Antonin Pascoali <gabrielantonin999@gmail.com>
2024-05-21 08:57:52 -0300

    Node - Aula 13: Express Middlewares

---
+++ node/aula13/src/middlewares/middleware.js
@@ -0,0 +1,8 @@
+exports.middlewareGlobal = (req, res, next) => {
+    if (req.body.cliente) {
+        console.log();
+        console.log(`Vi que você postou ${req.body.cliente}`);
+        console.log();
+    }
+    next();
+};
