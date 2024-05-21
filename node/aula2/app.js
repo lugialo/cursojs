deeefba2fdcba6990bc1ca4bf41eb8424312b66d
Gabriel Antonin Pascoali <103425929+lugialo@users.noreply.github.com>
2024-05-21 09:05:57 -0300

    Delete node/aula2 directory

--- node/aula2/app.js
+++
@@ -1,8 +0,0 @@
-const Cachorro = require('./mod');
-
-const cachorrinho = new Cachorro('dog');
-cachorrinho.latir();
-
-const path = require('path');
-console.log(path.resolve(__dirname, '..', '..', 'aula03'));
-console.log(__dirname);
--- node/aula2/mod.js
+++
@@ -1,9 +0,0 @@
-module.exports = class Cachrro {
-    constructor(nome) {
-        this.nome = nome;
-    }
-
-    latir() {
-        console.log(`${this.nome} está latindo!`);
-    }
-};
